242355181ffae0c4e07bfe72de18fbd699a292ab
Alireza Bagherzadeh <alirezabagherzadeh.chr@gmail.com>
2022-07-21 20:40:56 +0430

    Modal script optimized

--- components/Modal/script.js
+++ components/Modal/script.js
@@ -1,20 +1,23 @@
-function showModal() {
-  const modal = document.getElementById("modal");
-  const button = document.getElementById("theBtn");
-  const close = document.getElementById("close");
-  button.style.zIndex = "-1";
-  modal.style.display = "flex";
-  modal.style.animation = "popup 0.5s";
-  modal.style.backgroundColor = "rgba(128, 128, 128, 0.5)";
-  close.style.visibility = "visible";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  let modal = document.getElementById("modal");
+  let button = document.getElementById("theBtn");
+  let close = document.getElementById("close");
 
-function closeModal() {
-  const modal = document.getElementById("modal");
-  const button = document.getElementById("theBtn");
-  const close = document.getElementById("close");
-  button.style.zIndex = "1";
-  modal.style.display = "none";
-  modal.style.backgroundColor = "";
-  close.style.visibility = "hidden";
-}
+  function showModal() {
+    button.style.zIndex = "-1";
+    modal.style.display = "flex";
+    modal.style.animation = "popup 0.5s";
+    modal.style.backgroundColor = "rgba(128, 128, 128, 0.5)";
+    close.style.visibility = "visible";
+  }
+
+  function closeModal() {
+    button.style.zIndex = "1";
+    modal.style.display = "none";
+    modal.style.backgroundColor = "";
+    close.style.visibility = "hidden";
+  }
+
+  button.addEventListener("click", showModal);
+  close.addEventListener("click", closeModal);
+});
